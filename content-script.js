a5d956ff57b72eec7cd74a8fe4c80b16d6c6806c
Jon Seaton <43418410+JonSeaton93@users.noreply.github.com>
2024-05-29 17:18:55 -0700

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -57,14 +57,14 @@ chrome.storage.sync.get(['userData'], function(result) {
             if (willBlock)
             {
                 //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-                window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+                window.location.href = "https://www.google.com";
             }
         }
     }
     else
     {
         //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-        window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+        window.location.href = "https://www.google.com";
     }
 
     setInterval(deleteCommentAndLogo, 500)
